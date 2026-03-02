11f5823217f0c44e3c68f27652acd827ec287114
alexban94 <alex-h@live.co.uk>
2026-03-02 20:29:56 +0000

    Updated comments to custom exception class.

--- src/main/java/com/example/credit_service/exception/ApplicationNotFoundException.java
+++ src/main/java/com/example/credit_service/exception/ApplicationNotFoundException.java
@@ -1,9 +1,12 @@
 package com.example.credit_service.exception;
 
+// RuntimeException (unchecked) and does not require 'throws' in method signatures.
+// If the service throws this error it will propagate up the call stack until something handles it, in this case Spring
+// with @ControllerAdvice.
 public class ApplicationNotFoundException extends RuntimeException {
     public ApplicationNotFoundException(String id) {
-        // Call super class so error propagates.
-        // TODO: review super class RuntimeException functionality.
+        // Call super class constructor to set the erorr message stored in the exception.
+        // Error message indicates the given application ID does not exist in the database
         super("Application not found: " + id);
     }
 }
