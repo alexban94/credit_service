02cfbfd92a9bf84b2669c9cb56d39e979e93f49e
alexban94 <alex-h@live.co.uk>
2026-03-08 00:42:42 +0000

    Added files for JUnit test classes. TODO: implement tests.

--- src/test/java/com/example/credit_service/CreditRiskApplicationTests.java
+++ src/test/java/com/example/credit_service/CreditRiskApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CreditServiceApplicationTests {
+class CreditRiskApplicationTests {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/com/example/credit_service/controller/CreditApplicationControllerTests.java
@@ -0,0 +1,7 @@
+package com.example.credit_service.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CreditApplicationControllerTests {
+}
