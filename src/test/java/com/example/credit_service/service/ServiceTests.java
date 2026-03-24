6bc15468cd5d6a8c54fb08ca8d96b18a7594a261
alexban94 <alex-h@live.co.uk>
2026-03-24 23:58:38 +0000

    Completed approve/reject application unit tests.

--- src/test/java/com/example/credit_service/service/ServiceTests.java
+++ src/test/java/com/example/credit_service/service/ServiceTests.java
@@ -1,6 +1,7 @@
 package com.example.credit_service.service;
 
 import com.example.credit_service.dto.CreditRequest;
+import com.example.credit_service.dto.CreditResponse;
 import com.example.credit_service.repository.ApplicationRepo;
 import com.example.credit_service.repository.EventRepo;
 import com.example.credit_service.risk.RiskCalculator;
@@ -8,6 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 // Use Mockito in this test class to enable @Mock/@InjectMocks.
 @ExtendWith(MockitoExtension.class)
@@ -27,15 +34,67 @@ public class ServiceTests {
     @InjectMocks
     private CreditApplicationService service;
 
-    void shouldSubmitApplication(){
+    void shouldApproveApplication(){
 
         // Create an input - the request DTO, a submitted application.
         CreditRequest request = new CreditRequest("Pasta", "Bake",
-                "FFXIV", 50000, 15000);
+                "FFXIV", 50000, 10000);
 
+        // Define mock behaviour.
+        when(calc.calculateRiskScore(50000, 10000)).thenReturn(80); //return this when method is called with these inputs.
+        when(calc.decision(80)).thenReturn("APPROVED");
 
+        // accept any input and return it dynamically with thenAnswer.
+        // object passed into thenAnswer() is InvocationOnMock object. use it to get first argument and return it like the save method does.
+        when(appRepo.save(any())).thenAnswer(i -> i.getArgument(0));
 
+        // Call the method being tested.
+        CreditResponse response = service.submitApplication(request);
 
+        // Check if method output response has the correct values.
+        // Assertions. first param is message shown if it fails.
+        assertEquals("Decision should be APPROVED","APPROVED", response.decision());
+        assertEquals("Risk Score should be 80", response.riskScore(), 80);
+        assertNotNull("DTO appID should be set", response.appID());
+
+        // Verify - check methods were called on the mocks. Useful if bugs are introduced e.g. method call removed when modifying class.
+        verify(calc).calculateRiskScore(50000, 10000);
+        verify(calc).decision(80);
+        verify(appRepo).save(any());
+        verify((eventRepo)).save(any());
+
+   }
+
+    void shouldRejectApplication(){
+        CreditRequest request = new CreditRequest("Pasta", "Bake",
+                "FFXIV", 10000, 50000);
+
+        // Define mock behaviour.
+        when(calc.calculateRiskScore(10000, 50000)).thenReturn(10); //return this when method is called with these inputs.
+        when(calc.decision(10)).thenReturn("REJECTED");
+
+        // accept any input and return it dynamically with thenAnswer.
+        // object passed into thenAnswer() is InvocationOnMock object. use it to get first argument and return it like the save method does.
+        when(appRepo.save(any())).thenAnswer(i -> i.getArgument(0));
+
+        // Call the method being tested.
+        CreditResponse response = service.submitApplication(request);
+
+        // Check if method output response has the correct values.
+        // Assertions. first param is message shown if it fails.
+        assertEquals("Decision should be REJECTED","REJECTED", response.decision());
+        assertEquals("Risk Score should be 10", response.riskScore(), 80);
+        assertNotNull("DTO appID should be set", response.appID());
+
+        // Verify - check methods were called on the mocks. Useful if bugs are introduced e.g. method call removed when modifying class.
+        verify(calc).calculateRiskScore(10000, 50000);
+        verify(calc).decision(10);
+        verify(appRepo).save(any());
+        verify((eventRepo)).save(any());
+
+    }
+
+    void shouldThrowErrorInvalidApplication(){
 
     }
 
