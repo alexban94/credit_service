3cced5fddf19f137c6de379db4ec2cbd2af98dae
alexban94 <alex-h@live.co.uk>
2026-03-25 01:05:42 +0000

    Completed serviceTests.

--- src/test/java/com/example/credit_service/service/ServiceTests.java
+++ src/test/java/com/example/credit_service/service/ServiceTests.java
@@ -2,17 +2,22 @@ package com.example.credit_service.service;
 
 import com.example.credit_service.dto.CreditRequest;
 import com.example.credit_service.dto.CreditResponse;
+import com.example.credit_service.exception.ApplicationNotFoundException;
+import com.example.credit_service.model.CustomerApplication;
 import com.example.credit_service.repository.ApplicationRepo;
 import com.example.credit_service.repository.EventRepo;
 import com.example.credit_service.risk.RiskCalculator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
@@ -34,6 +39,7 @@ public class ServiceTests {
     @InjectMocks
     private CreditApplicationService service;
 
+    @Test
     void shouldApproveApplication(){
 
         // Create an input - the request DTO, a submitted application.
@@ -60,11 +66,12 @@ public class ServiceTests {
         // Verify - check methods were called on the mocks. Useful if bugs are introduced e.g. method call removed when modifying class.
         verify(calc).calculateRiskScore(50000, 10000);
         verify(calc).decision(80);
-        verify(appRepo).save(any());
-        verify((eventRepo)).save(any());
+        verify(appRepo, times(1)).save(any());
+        verify(eventRepo, times(1)).save(any());
 
    }
 
+    @Test
     void shouldRejectApplication(){
         CreditRequest request = new CreditRequest("Pasta", "Bake",
                 "FFXIV", 10000, 50000);
@@ -89,16 +96,49 @@ public class ServiceTests {
         // Verify - check methods were called on the mocks. Useful if bugs are introduced e.g. method call removed when modifying class.
         verify(calc).calculateRiskScore(10000, 50000);
         verify(calc).decision(10);
-        verify(appRepo).save(any());
-        verify((eventRepo)).save(any());
+        // Check they have saved only once.
+        verify(appRepo, times(1)).save(any());
+        verify(eventRepo, times(1)).save(any());
 
     }
 
-    void shouldThrowErrorInvalidApplication(){
-
-    }
-
+    @Test
     void shouldGetApplication(){
+        //Test for a valid application that exists in the database.
+        CustomerApplication app = new CustomerApplication("TestID", "Pasta", "Bake", "FFXIV", 10000, 50000, 80, "APPROVED");
+
+        when(appRepo.findById("TestID")).thenReturn(Optional.of(app));
+
+        CreditResponse response = service.getApplication("TestID");
+
+        assertEquals(null,"TestID", response.appID());
+        assertEquals(null, "APPROVED", response.decision());
+        assertEquals(null, 80, response.riskScore());
+
+        // Check repo was queried by ID in the method we are testing.
+        verify(appRepo).findById("TestID");
+
+        // If application exists it should
+    }
+
+    @Test
+    void shouldGetApplicationError(){
+        //findByID method implemented by SpringData returns Optional container object instead of null.
+        // safer to use and indicates it may or may not return a CustomerApplication.
+        when(appRepo.findById("TestID")).thenReturn(Optional.empty());
+
+        //assertThrows requires a lambda function as an Executable type, so it controls when it runs and when the exception is thrown.
+        // compared against passing in service.getApplication() directly, the method will run immediately and
+        // exception is thrown before assertThrows even sees it. with this, it can call executable.execute() function to run it.
+        // assertThrows asserts that it throws a specific exception matching the one provided.
+        // save it here in a variable so we can also test the error message.
+        ApplicationNotFoundException e = assertThrows(ApplicationNotFoundException.class,
+                () -> service.getApplication("TestID")
+        );
+
+        assertEquals("Exception error message does not match", "Application not found: TestID", e.getMessage());
+
+        verify(appRepo, times(1)).findById("TestID");
 
     }
 
