7f5c07848af89c4da61e2766eb87620876754c46
alexban94 <alex-h@live.co.uk>
2026-04-04 23:58:07 +0100

    Added new test classes

---
+++ src/test/java/com/example/credit_service/event/EventTests.java
@@ -0,0 +1,9 @@
+package com.example.credit_service.event;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+// Use Mockito in this test class to enable @Mock/@InjectMocks.
+@ExtendWith(MockitoExtension.class)
+public class EventTests {
+}
