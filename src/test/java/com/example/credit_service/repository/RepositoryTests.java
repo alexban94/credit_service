ffa380a34f6714ad699371f99140dac9cff813cf
alexban94 <alex-h@live.co.uk>
2026-04-05 23:39:05 +0100

    Added method stubs.

--- src/test/java/com/example/credit_service/repository/RepositoryTests.java
+++ src/test/java/com/example/credit_service/repository/RepositoryTests.java
@@ -1,9 +1,22 @@
 package com.example.credit_service.repository;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 // Use Mockito in this test class to enable @Mock/@InjectMocks.
 @ExtendWith(MockitoExtension.class)
 public class RepositoryTests {
+
+    //Test finding unprocessed events
+    @Test
+    void shouldFindByProcessedFalse(){
+
+    }
+
+    //Test when no results (Optional object should indicate empty).
+    @Test
+    void whenNoResultsFindByProcessedFalse(){
+
+    }
 }
